5e5573b0904633ec1c9ccffa65610ec957c59ed3
Beto Martinez <betoml5@hotmail.com>
2020-09-26 00:22:16 -0500

    Almost done

--- index.js
+++ index.js
@@ -1,58 +1,38 @@
-const input_ip = document.querySelector('#input_ip').value;
-const btn_submit = document.querySelector('#btn_ip');
-const api = 'https://geo.ipify.org/api/v1?apiKey=at_IGgfN6y8XuYIAyf90S4pk9l3OlLfY&ipAddress=';
-const tilesProvider = '	https://a.tile.openstreetmap.org/{z}/{x}/{y}.png'
+let input_ip = document.querySelector("#input_ip");
+const btn_submit = document.querySelector("#btn_ip");
+const api =
+  "https://geo.ipify.org/api/v1?apiKey=at_IGgfN6y8XuYIAyf90S4pk9l3OlLfY&ipAddress=";
+const tilesProvider = "	https://a.tile.openstreetmap.org/{z}/{x}/{y}.png";
 
-
-
-
-
-
-async function getLocation(api, ip){
-    const res = await fetch(`${api}${ip}`)
+async function getLocation(api, ip) {
+  try {
+    const res = await fetch(`${api}${ip}`);
     const data = await res.json();
+    const coords = [data.location.lat, data.location.lng];
+    mymap.setView(coords);
+    const marker = L.marker(coords).bindPopup("You are here!");
+    mymap.addLayer(marker);
     console.log(data);
     return data;
-    
-
+  } catch (e) {
+    console.error(e);
+  }
 }
 
-btn_submit.addEventListener('click', () => {
-    console.log('Clicking');
-    getLocation(api, input_ip);
-    btn_submit.re
+btn_submit.addEventListener("click", () => {
+  getLocation(api, input_ip.value);
 });
 
-
-
-
-// let mymap = L.map('mapid').setView([51.505, -0.09], 13);
+let mymap = L.map("mapid").setView([51.505, -0.09], 13);
 L.tileLayer(tilesProvider, {
-    maxZoom: 18
-}).addTo(mymap)
+  maxZoom: 18,
+}).addTo(mymap);
 
+mymap.locate({ enableHighAccuracy: true });
 
-
-
-// const popup = L.popup();
-// const onMapClick = e => {
-//     popup
-//     .setLatLng(e.latlng)
-//     .setContent("You clicked the map at " + e.latlng.toString())
-//     .openOn(mymap);
-// }
-
-// mymap.on('click', onMapClick)
-
-
-// const marker  = L.marker([51.505, -0.09]).bindPopup('Hey there');
-// mymap.addLayer(marker)
-
-// mymap.locate({enableHighAccuracy: true})
-
-// mymap.on('locationfound', e => {
-//     const coords = [e.latlng.lat, e.latlng.lng]
-//     const marker  = L.marker(coords).bindPopup('You are here!');
-//     mymap.addLayer(marker)
-// })
-
+mymap.on("locationfound", (e) => {
+  const coords = [e.latlng.lat, e.latlng.lng];
+  mymap.setView(coords);
+  const marker = L.marker(coords).bindPopup("You are here!");
+  mymap.addLayer(marker);
+});
